03597c4a845d093a716eef8dfffdfe7c5509ba3d
Quang Nguyen <nnquang.1412@gmail.com>
2021-06-19 10:08:20 +0700

    .

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/TasksList.css'
 
 const Task = (props) => {
+    const [newTaskname, setNewTaskname] = useState("")
+    const [toggleUpdate, setToggleUpdate] = useState(false)
+    // !toggleUpdate = not toggleUpdate
     return (
         <div className='task'>
-            This is {props.name}
+            {props.name}
+            <button onClick={props.handleDelete}>Delete</button>
+            <button onClick={() => setToggleUpdate(!toggleUpdate)}>Update</button>
+            { toggleUpdate ? (
+                <>
+                    <input value={newTaskname} type="text" onChange={(e) => setNewTaskname(e.target.value)}/>
+                    <button onClick={() => props.handleUpdate(props.id, newTaskname)}>Save Update</button>
+                </>
+            ) : null}
         </div>
     )
 }
--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -34,7 +34,6 @@ const TasksList = () => {
             finished: false
         }
         
-        
         // adding new task to tasks list (state variable)
         setTasks([...tasks, newTask])
         setTaskname("") 
@@ -42,6 +41,20 @@ const TasksList = () => {
         // let copyTasks = tasks
         // copyTasks.push(newTask)
         // setTasks(copyTasks)
+    }
+
+    // called when delete button is pressed
+    const handleDelete = (deleteId) => {
+        const updated_list = tasks.filter((task) => task.id != deleteId)
+        setTasks(updated_list)
+    }
+
+    const handleUpdate = (updateId, updateName) => {
+        const updateTaskIndex = tasks.findIndex((task) => task.id == updateId)
+        const newTasks = [...tasks] //deep copy
+        console.log(newTasks[updateTaskIndex])
+        newTasks[updateTaskIndex].name = updateName
+        setTasks(newTasks)
     }
     return (
         <div>
@@ -56,6 +69,14 @@ const TasksList = () => {
                 Add
             </button>
 
+            {/* { a ? b : c  } */}
+
+            {/* {if (a) {
+                b
+            }else{
+                c
+            }} */}
+
 
             <h2>Tasks List</h2>
             <div className="task-container">  
@@ -63,7 +84,12 @@ const TasksList = () => {
                 {tasks.map(
                     (task) => {
                     return (
-                        <Task id={task.id} name={task.name}/>
+                        <Task 
+                            id={task.id} 
+                            name={task.name}
+                            handleDelete={() => handleDelete(task.id)}
+                            handleUpdate={handleUpdate}
+                            />
                     )
                 })
                 }
